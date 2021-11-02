b4a924b65b4e6f5986c70b4a18ce9ecd8757e78a
Stuart Bradley <stuart@juno.io>
2021-11-02 22:06:08 +0000

    Models added

---
+++ NetC.Domain/BlogPost.cs
@@ -0,0 +1,40 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetC.Domain
+{
+    public class BlogPost
+    {
+        public int Id { get; }
+        public DateTime Date { get; }
+        public string Title { get; }
+        public string Image { get; }
+        public string HtmlContext { get; }
+        private List<Comment> _comments = new List<Comment>();
+        public IReadOnlyList<Comment> Comments => _comments?.AsReadOnly();
+
+        [JsonConstructor]
+        public BlogPost(int id, DateTime date, string title, string image, string htmlContent, List<Comment> comments)
+        {
+            Id = id;
+            Date = date;
+            Title = title;
+            Image = image;
+            HtmlContext = htmlContent;
+            _comments = comments;
+        }
+
+        public int AddComment(int id, string name, string emailAddress, string comment, DateTime creationDate)
+        {
+            if (id == 0)
+                id = _comments.Max(x => x.Id) + 1;
+            _comments.Add(new Comment(id, name, emailAddress, comment, creationDate));
+
+            return id;
+        }
+    }
+}
---
+++ NetC.JuniorDeveloperExam.Web/Controllers/BlogPostsController.cs
@@ -0,0 +1,42 @@
+﻿using MediatR;
+using NetC.Application.Queries;
+using NetC.JuniorDeveloperExam.Web.Models.BlogPosts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+
+namespace NetC.JuniorDeveloperExam.Web.Controllers
+{
+    public class BlogPostsController:Controller
+    {
+        private readonly IMediator _mediatr;
+
+        public BlogPostsController(IMediator mediatr)
+        {
+            _mediatr = mediatr;
+        }
+        public async Task<ActionResult> Index(int id)
+        {
+            var blogPost = await _mediatr.Send(new GetBlogPostByIdQuery(id));
+            BlogPostModel model = new BlogPostModel()
+            {
+                Id = blogPost.Id,
+                Date = blogPost.Date,
+                HtmlContext = blogPost.HtmlContext,
+                Image = blogPost.Image,
+                Title = blogPost.Title,
+                Comments = blogPost.Comments?.Select(x => new CommentModel()
+                {
+                    Name = x.Name,
+                    CreationDate = x.CreationDate,
+                    EmailAddress = x.EmailAddress,
+                    Message = x.Message
+                }).ToList()
+            };
+            return View(model);
+        }
+    }
+}
